5e93c8f64404c02c0368f7faf97e2475a1eb4096
Thanasis Mitragkas <55363474+sAsPeCt488@users.noreply.github.com>
2021-12-01 18:22:31 +0200

    Add Part 2

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,23 @@
-increased = 0
 
 with open("input.txt", "r") as input:
     measurements = [int(num) for num in input.read().splitlines()]
 
-for i in range(1, len(measurements)):
-    if measurements[i-1] < measurements[i]:
-        increased += 1
-print("[+] Result: ", increased)
+
+def part1():
+    inc_counter = 0
+    for i in range(1, len(measurements)):
+        if measurements[i-1] < measurements[i]:
+            inc_counter += 1
+    return inc_counter
+
+
+def part2():
+    inc_counter = 0
+    for i in range(len(measurements) - 3):
+        if measurements[i] < measurements[i + 3]:
+            inc_counter += 1
+    return inc_counter
+
+
+print("[+] Part 1  - Result: ", part1())
+print("[+] Part 2  - Result: ", part2())
